40fc3e71e108577ccdcd59c2bb14a0530d9be4c9
mohitRauniyar <learning.mohitrauniyar.01@gmail.com>
2025-02-08 17:31:41 +0530

    Add company routes and utility functions

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import { getCompanyProfile, updatePassword, updateProfile } from '../controllers/company.controller';
 
 const router = express.Router()
+
+router.get("/:username", getCompanyProfile)
+router.update("/:username/update", updateProfile)
+router.update("/:username/password/update", updatePassword)
+router.post("/:username/post/create", updatePassword)
+router.update("/:username/post/update", updatePassword)
+
 
 module.exports = router;
